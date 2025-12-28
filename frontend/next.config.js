67fe6996275f51ef41d0606b0d2d0160fc42f88a
JohnsonNguyen-coll <hoangnl1811@gmail.com>
2025-12-28 21:17:47 +0700

    fix: add missing optional dependencies for MetaMask SDK

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack');
+
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    
+    // Ignore optional dependencies and React Native modules not needed for web
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pino-pretty$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^@react-native-async-storage\/async-storage$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^react-native$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^@react-native/,
+      })
+    );
+    
     return config;
   },
 }
